e38ea37e14177c5f62ed6d11b950e4e4bd9acb65
Petr Prihoda <petr.prihoda@plus4u.net>
2025-11-01 11:47:06 +0100

    added simple error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { RoutePrefetcher } from "./components/utils/RoutePrefetcher";
 import { Top } from "./components/layout/Top";
+import { ErrorBoundary } from "./components/ui/ErrorBoundary";
 import { PageWrapper, ContentWrapper } from "./App.styles";
 
 function App() {
   return (
-    <PageWrapper>
-      <Top />
-      <ContentWrapper>
-        <RoutePrefetcher />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </ContentWrapper>
-    </PageWrapper>
+    <ErrorBoundary>
+      <PageWrapper>
+        <Top />
+        <ContentWrapper>
+          <RoutePrefetcher />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </ContentWrapper>
+      </PageWrapper>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/ui/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import React, { Component, type ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error('ErrorBoundary caught an error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+          <h2>Something went wrong</h2>
+          <p>Please refresh the page.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
